910816afba74e5e979c4e4a5a8cb0025b4165531
erdey <erdeysyoum@gmail.com>
2024-07-11 09:23:40 +0000

    create users table with country enumeration

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- create a table with constraints
+
+CREATE TABLE IF NOT EXISTS users (
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM(US, CO, TN) DEFAULT US
+	);
