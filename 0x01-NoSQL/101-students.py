909e851d7cde5fde6c276e3f14f00a6be2a54e1c
erdey <erdeysyoum@gmail.com>
2024-07-17 05:09:20 +0000

    sort documents

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -4,4 +4,10 @@
 
 def top_students(mongo_collection):
     """ returns all students sorted by average score """
-    return mongo_collection.find().sort({average: 1})
+    return mongo_collection.aggregate([
+        {"$project": {
+            "name": "$name",
+            "averageScore": {"$avg": "$topics.score"}
+        }},
+        {"$sort": {"averageScore": -1}}
+    ])
